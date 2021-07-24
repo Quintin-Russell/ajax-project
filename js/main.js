0a31df0b9b1321bd1f0c5b4509fe7667cd33401f
Quintin Russell <quinn.j.russell@gmail.com>
2021-07-23 22:51:43 -0700

    advanced formatting of grateful screen and added media queries

--- js/main.js
+++ js/main.js
@@ -3,11 +3,40 @@ const $newJournalButton = document.querySelector('[button="new-journal"]');
 // const $graphButton = document.querySelector('[button="graph"]');
 const $homeDiv = document.querySelector('[data-view = "home-screen"]');
 const $gratefulDiv = document.querySelector('[data-view = "grateful"]');
+const $headerUl = document.querySelector('.header-list');
+const $headerLogo = document.querySelector('#header-logo');
+const $dateH2 = document.querySelector('[data="current-date"]');
+
+function showPage(show, hide) {
+  show.setAttribute('class', 'container');
+  hide.setAttribute('class', 'hidden');
+}
+
+function getDate() {
+  const monthArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const date = new Date();
+  let month = date.getMonth();
+  month = monthArr[month];
+  let day = date.getDate();
+  day = day.toString();
+  let year = date.getFullYear();
+  year = year.toString();
+  const formattedYear = month + ' ' + day + ', ' + year;
+  return formattedYear;
+}
 
 $newJournalButton.addEventListener('click', function (e) {
-  $homeDiv.setAttribute('class', 'hidden container');
-  $gratefulDiv.setAttribute('class', 'flex container');
+  showPage($gratefulDiv, $homeDiv);
+  const $nJHeader = document.createElement('li');
+  const $nJHeaderH2 = document.createElement('h2');
+  $nJHeaderH2.textContent = 'New Journal Entry';
+  $nJHeader.appendChild($nJHeaderH2);
+  $headerUl.appendChild($nJHeader);
+  $nJHeaderH2.setAttribute('class', 'new-journal-header work-sans');
+  $headerLogo.setAttribute('class', 'header-logo work-sans');
+  $dateH2.textContent = 'Date: ' + getDate();
 });
+
 /*
 $draftButton.addEventListener('click', function (e) {
   $homeDiv.setAttribute('class', 'hidden container');
