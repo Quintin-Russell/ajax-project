c13af112a3aac375dae33919308bf07e4ce4838c
Quintin Russell <quinn.j.russell@gmail.com>
2021-08-05 20:17:16 -0700

    finished issue 1

--- js/data.js
+++ js/data.js
@@ -1,38 +1,39 @@
-// /* exported data */
-// const entries = [];
-// const drafts = [];
-// const quotes = {
-//   'P+': ['Begin at once to live, and count each separate day as a separate life.—Seneca', 'It looks like you have some momentum today! Keep it up! Do something today that your future self will thank you for!', "Spread the positivity you have: tell someone they look nice today, talk to that person you catch eyes with at the grocery store, help someone carry something heavy... Be a part of someone else's day!"],
-//   P: ['What man actually needs is not a tensionless state but rather the striving and struggling for some goal worthy of him.” – Viktor Frankl. What are you working toward today', ''],
-//   NEU: ["Luck is what happens when preparation meets opportunity.―Seneca. Don't forget to do something that your future self will thank you for"],
-//   N: ['Focus on what you can control, the rest will fall into place', 'If you want to improve, be content to be thought foolish and stupid.―Epictetus', "Remember what you're greatful for", "The winners don't let the haters effect their tragectory"],
-//   'N+': ['The key is to keep company only with people who uplift you, whose presence calls forth your best.―Epictetus...Call someone and tell them you love them, tell them about what you have going on in your life. Lean on those around you for support', '“You will never see me surrender, never see me cry, but you will often see me walk away. Turn around and just leave, without looking back.” – Charlotte Eriksson', "If you want to improve, be content to be thought foolish and stupid.―Epictetus. Some people aren't going to understand you, that doesn't mean that you have to listen to them when they tell you 'no'.", "Keep your head up. You've been through worse. Everything (even this is temporary)", "This too shall pass...Remember what your greatful for and that someone, somewhere would LOVE to have your 'bad day'"]
-// };
-// // eslint-disable-next-line prefer-const
-// let currentObj = null;
+/* exported data */
+const entries = [];
+const drafts = [];
+// eslint-disable-next-line no-unused-vars
+const quotes = {
+  'P+': ['Begin at once to live, and count each separate day as a separate life.—Seneca', 'It looks like you have some momentum today! Keep it up! Do something today that your future self will thank you for!', "Spread the positivity you have: tell someone they look nice today, talk to that person you catch eyes with at the grocery store, help someone carry something heavy... Be a part of someone else's day!"],
+  P: ['What man actually needs is not a tensionless state but rather the striving and struggling for some goal worthy of him.” – Viktor Frankl. What are you working toward today', ''],
+  NEU: ["Luck is what happens when preparation meets opportunity.―Seneca. Don't forget to do something that your future self will thank you for"],
+  N: ['Focus on what you can control, the rest will fall into place', 'If you want to improve, be content to be thought foolish and stupid.―Epictetus', "Remember what you're greatful for", "The winners don't let the haters effect their tragectory"],
+  'N+': ['The key is to keep company only with people who uplift you, whose presence calls forth your best.―Epictetus...Call someone and tell them you love them, tell them about what you have going on in your life. Lean on those around you for support', '“You will never see me surrender, never see me cry, but you will often see me walk away. Turn around and just leave, without looking back.” – Charlotte Eriksson', "If you want to improve, be content to be thought foolish and stupid.―Epictetus. Some people aren't going to understand you, that doesn't mean that you have to listen to them when they tell you 'no'.", "Keep your head up. You've been through worse. Everything (even this is temporary)", "This too shall pass...Remember what your greatful for and that someone, somewhere would LOVE to have your 'bad day'"]
+};
 
-// // eslint-disable-next-line no-unused-vars
-// function Entry() {
-//   this.title = undefined;
-//   this.formattedDate = undefined;
-//   this.fiveThings = [];
-//   this.text = undefined;
-//   this.response = undefined;
-//   this.score = undefined;
-//   this.scoreNum = undefined;
-// }
+let currentObj = null;// eslint-disable-line no-unused-vars, prefer-const
 
-// var previousEntries = localStorage.getItem('entries');
-// // eslint-disable-next-line no-unused-vars
-// previousEntries = JSON.parse(previousEntries);
+// eslint-disable-next-line no-unused-vars
+function Entry() {
+  this.title = undefined;
+  this.formattedDate = undefined;
+  this.fiveThings = [];
+  this.text = undefined;
+  this.response = undefined;
+  this.score = undefined;
+  this.scoreNum = undefined;
+}
 
-// var previousDrafts = localStorage.getItem('drafts');
-// // eslint-disable-next-line no-unused-vars
-// previousDrafts = JSON.parse(previousDrafts);
+var previousEntries = localStorage.getItem('entries');
+// eslint-disable-next-line no-unused-vars
+previousEntries = JSON.parse(previousEntries);
 
-// window.addEventListener('beforeunload', function (event) {
-//   const entriesJSON = JSON.stringify(entries);
-//   const draftsJSON = JSON.stringify(drafts);
-//   localStorage.setItem('entries', entriesJSON);
-//   localStorage.setItem('drafts', draftsJSON);
-// });
+var previousDrafts = localStorage.getItem('drafts');
+// eslint-disable-next-line no-unused-vars
+previousDrafts = JSON.parse(previousDrafts);
+
+window.addEventListener('beforeunload', function (event) {
+  const entriesJSON = JSON.stringify(entries);
+  const draftsJSON = JSON.stringify(drafts);
+  localStorage.setItem('entries', entriesJSON);
+  localStorage.setItem('drafts', draftsJSON);
+});
--- js/main.js
+++ js/main.js
@@ -1,206 +1,227 @@
-// const $newJournalButton = document.querySelector('#new-journal');
+const $newJournalButton = document.querySelector('#new-journal');
 // const $draftButton = document.querySelector('#drafts');
 // const $graphButton = document.querySelectorAll('[button = "view-graph"]');
-// const $homeDiv = document.querySelector('[data-view = "home-screen"]');
-// const $gratefulDiv = document.querySelector('[data-view = "grateful"]');
-// const $5thingsForm = document.getElementById('five-things');
-// const $journalTextForm = document.querySelector('#journal-cont');
-// const $headerUl = document.querySelector('.header-list');
-// const $headerLogo = document.querySelector('#header-logo');
-// const $dateH2 = document.querySelectorAll('[data="date"]');
-// const $saveDraftButton = document.querySelectorAll('[button = "save-draft"]');
-// const $nJContButton = document.querySelector('[button = "new-journal-cont"]');
-// const $grateful1 = document.getElementById('grateful1');
-// const $grateful2 = document.getElementById('grateful2');
-// const $grateful3 = document.getElementById('grateful3');
-// const $grateful4 = document.getElementById('grateful4');
-// const $grateful5 = document.getElementById('grateful5');
-// const $fiveThings = [$grateful1, $grateful2, $grateful3, $grateful4, $grateful5];
-// const $NJDiv = document.querySelector('[data-view="new-journal"]');
-// const $NJTextCont = document.querySelector('#journal-cont-text');
-// const $doneButton = document.querySelector('[button="done"]');
-// const $modalDiv = document.querySelector('[data-view="done-modal"]');
-// const $scoreH1 = document.querySelector('[data="modal-score"]');
-// const $modalQuoteP = document.querySelector('[data="modal-quote"]');
-// const $homeButton = document.querySelector('[button="home"]');
-// const $pgList = [$gratefulDiv, $NJDiv, $modalDiv];
-// let date;
-// let formattedDate;
+const $homeDiv = document.querySelector('[data-view = "home-screen"]');
+const $gratefulDiv = document.querySelector('[data-view = "grateful"]');
+const $5thingsForm = document.getElementById('five-things');
+const $journalTextForm = document.querySelector('#journal-cont');
+const $headerUl = document.querySelector('.header-list');
+const $headerLogo = document.querySelector('#header-logo');
+const $dateH2 = document.querySelectorAll('[data="date"]');
+const $saveDraftButton = document.querySelectorAll('[button = "save-draft"]');
+const $nJContButton = document.querySelector('[button = "new-journal-cont"]');
+const $grateful1 = document.getElementById('grateful1');
+const $grateful2 = document.getElementById('grateful2');
+const $grateful3 = document.getElementById('grateful3');
+const $grateful4 = document.getElementById('grateful4');
+const $grateful5 = document.getElementById('grateful5');
+const $fiveThings = [$grateful1, $grateful2, $grateful3, $grateful4, $grateful5];
+const $NJDiv = document.querySelector('[data-view="new-journal"]');
+const $NJTextCont = document.querySelector('#journal-cont-text');
+const $doneButton = document.querySelector('[button="done"]');
+const $modalDiv = document.querySelector('[data-view="done-modal"]');
+const $scoreH1 = document.querySelector('[data="modal-score"]');
+const $modalQuoteP = document.querySelector('[data="modal-quote"]');
+const $homeButton = document.querySelector('[button="home"]');
+const $pgList = [$gratefulDiv, $NJDiv, $modalDiv];
+let date;
+let formattedDate;
 
-// function showPage(show, hide) {
-//   show.setAttribute('class', 'container');
-//   hide.setAttribute('class', 'hidden');
-// }
+function showPage(show, hide) {
+  show.setAttribute('class', 'container');
+  hide.setAttribute('class', 'hidden');
+}
 
-// function getDate() {
-//   const monthArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-//   date = new Date();
-//   let month = date.getMonth();
-//   month = monthArr[month];
-//   let day = date.getDate();
-//   day = day.toString();
-//   let year = date.getFullYear();
-//   year = year.toString();
-//   formattedDate = month + ' ' + day + ', ' + year;
-// }
+function getDate() {
+  const monthArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  date = new Date();
+  let month = date.getMonth();
+  month = monthArr[month];
+  let day = date.getDate();
+  day = day.toString();
+  let year = date.getFullYear();
+  year = year.toString();
+  formattedDate = month + ' ' + day + ', ' + year;
+}
 
-// function removePageID() {
-//   if (($headerUl.childNodes) > 3) {
-//     const $pgID = document.querySelector('[data = "pg-ID"]');
-//     $headerUl.removeChild($pgID);
-//   }
-// }
+function removePageID() {
+  if (($headerUl.childNodes) > 3) {
+    const $pgID = document.querySelector('[data = "pg-ID"]');
+    $headerUl.removeChild($pgID);
+  }
+}
 
-// function getScoreNum(score) {
-//   if (score === 'P+') {
-//     currentObj.scoreNum = 2;
-//   } else if (score === 'P') {
-//     currentObj.scoreNum = 1;
-//   } else if (score === 'NEU') {
-//     currentObj.scoreNum = 0;
-//   } else if (score === 'N') {
-//     currentObj.scoreNum = -1;
-//   } else if (score === 'N+') {
-//     currentObj.scoreNum = -2;
-//   } else {
-//     currentObj.scoreNum = null;
-//   }
-// }
+function getScoreNum(score) {
+  if (score === 'P+') {
+    // eslint-disable-next-line no-undef
+    currentObj.scoreNum = 2;
+  } else if (score === 'P') {
+    // eslint-disable-next-line no-undef
+    currentObj.scoreNum = 1;
+  } else if (score === 'NEU') {
+    // eslint-disable-next-line no-undef
+    currentObj.scoreNum = 0;
+  } else if (score === 'N') {
+    // eslint-disable-next-line no-undef
+    currentObj.scoreNum = -1;
+  } else if (score === 'N+') {
+    // eslint-disable-next-line no-undef
+    currentObj.scoreNum = -2;
+  } else {
+    // eslint-disable-next-line no-undef
+    currentObj.scoreNum = null;
+  }
+}
 
-// function randQuote(score) {
-//   let quote = '';
-//   for (const key in quotes) {
-//     if (key === score) {
-//       const quoteArr = quotes[key];
-//       const index = Math.floor(Math.random() * (quoteArr.length - 1));
-//       quote = quoteArr[index];
-//       return quote;
-//     }
-//   }
-// }
+function randQuote(score) {
+  let quote = '';
+  // eslint-disable-next-line no-undef
+  for (const key in quotes) {
+    if (key === score) {
+      // eslint-disable-next-line no-undef
+      const quoteArr = quotes[key];
+      const index = Math.floor(Math.random() * (quoteArr.length - 1));
+      quote = quoteArr[index];
+      return quote;
+    }
+  }
+}
 
-// function afterAPI() {
-//   $5thingsForm.reset();
-//   $journalTextForm.reset();
-//   getScoreNum(currentObj.score);
-//   const modalScore = `Your Score: ${currentObj.scoreNum}`;
-//   const quote = randQuote(currentObj.score);
-//   $scoreH1.textContent = modalScore;
-//   $modalQuoteP.textContent = quote;
-//   removePageID();
-//   showPage($modalDiv, $NJDiv);
-// }
+function afterAPI() {
+  $5thingsForm.reset();
+  $journalTextForm.reset();
+  // eslint-disable-next-line no-undef
+  getScoreNum(currentObj.score);
+  // eslint-disable-next-line no-undef
+  const modalScore = `Your Score: ${currentObj.scoreNum}`;
+  // eslint-disable-next-line no-undef
+  const quote = randQuote(currentObj.score);
+  $scoreH1.textContent = modalScore;
+  $modalQuoteP.textContent = quote;
+  removePageID();
+  showPage($modalDiv, $NJDiv);
+}
 
-// function sendMoodReq(text) {
-//   const formdata = new FormData();
-//   formdata.append('key', 'e599b98b4c266944eb2b0f2ada2724cc');
-//   formdata.append('txt', text);
-//   formdata.append('lang', 'auto');
+function sendMoodReq(text) {
+  const formdata = new FormData();
+  formdata.append('key', 'e599b98b4c266944eb2b0f2ada2724cc');
+  formdata.append('txt', text);
+  formdata.append('lang', 'auto');
 
-//   const requestOptions = {
-//     method: 'POST',
-//     body: formdata,
-//     redirect: 'follow'
-//   };
+  const requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+  };
 
-//   fetch('https://api.meaningcloud.com/sentiment-2.1', requestOptions)
-//     .then(response => response.json())
-//     .then(responseJSON => {
-//       currentObj.response = responseJSON;
-//       currentObj.score = currentObj.response.score_tag;
-//       afterAPI();
-//     })
-//     .catch(error => window.alert("Don't know what happened there, but something went wrong. Please try to submit again"));
-// }
+  fetch('https://api.meaningcloud.com/sentiment-2.1', requestOptions)
+    .then(response => response.json())
+    .then(responseJSON => {
+      // eslint-disable-next-line no-undef
+      currentObj.response = responseJSON;
+      // eslint-disable-next-line no-undef
+      currentObj.score = currentObj.response.score_tag;
+      afterAPI();
+    })
+    // eslint-disable-next-line node/handle-callback-err
+    .catch(error => window.alert("Don't know what happened there, but something went wrong. Please try to submit again"));
+}
 
-// // home page eventListeners
-// $newJournalButton.addEventListener('click', function (e) {
-//   showPage($gratefulDiv, $homeDiv);
-//   const $nJHeader = document.createElement('li');
-//   const $nJHeaderH2 = document.createElement('h2');
-//   getDate();
-//   $nJHeaderH2.textContent = 'New Journal Entry';
-//   $nJHeader.setAttribute('data', 'pg-ID');
-//   $nJHeader.appendChild($nJHeaderH2);
-//   $headerUl.appendChild($nJHeader);
-//   $nJHeaderH2.setAttribute('class', 'new-journal-header work-sans');
-//   $headerLogo.setAttribute('class', 'header-logo work-sans');
-//   let item;
-//   for (item of $dateH2) {
-//     item.textContent = 'Date: ' + formattedDate;
-//   }
-// });
-// /* $draftButton.addEventListener('click', function (e) {
-//   $homeDiv.setAttribute('class', 'hidden container');
-//   $draftDiv.setAttribute('class', 'flex container');
-// });
+// home page eventListeners
+$newJournalButton.addEventListener('click', function (e) {
+  showPage($gratefulDiv, $homeDiv);
+  const $nJHeader = document.createElement('li');
+  const $nJHeaderH2 = document.createElement('h2');
+  getDate();
+  $nJHeaderH2.textContent = 'New Journal Entry';
+  $nJHeader.setAttribute('data', 'pg-ID');
+  $nJHeader.appendChild($nJHeaderH2);
+  $headerUl.appendChild($nJHeader);
+  $nJHeaderH2.setAttribute('class', 'new-journal-header work-sans');
+  $headerLogo.setAttribute('class', 'header-logo work-sans');
+  let item;
+  for (item of $dateH2) {
+    item.textContent = 'Date: ' + formattedDate;
+  }
+});
+/* $draftButton.addEventListener('click', function (e) {
+  $homeDiv.setAttribute('class', 'hidden container');
+  $draftDiv.setAttribute('class', 'flex container');
+});
 
-// $graphButton.addEventListener('click', function (e) {
-//   $homeDiv.setAttribute('class', 'hidden container');
-//   $graphDiv.setAttribute('class', 'flex container');
-// });
-// */
-// // gratefulDiv eventListeners
-// $gratefulDiv.addEventListener('click', function (e) {
-//   for (const sm of $saveDraftButton) {
-//     if (e.target === sm) {
-//       e.preventDefault();
-//       const newEntry = new Entry();
-//       newEntry.title = date;
-//       for (const item of $fiveThings) {
-//         const txt = item.value;
-//         newEntry.fiveThings.push(txt);
-//         newEntry.formattedDate = formattedDate;
-//       }
-//       drafts.push(newEntry);
-//       showPage($homeDiv, $gratefulDiv);
-//       $5thingsForm.reset();
-//       break;
-//     }
-//   }
-// });
+$graphButton.addEventListener('click', function (e) {
+  $homeDiv.setAttribute('class', 'hidden container');
+  $graphDiv.setAttribute('class', 'flex container');
+});
+*/
+// gratefulDiv eventListeners
+$gratefulDiv.addEventListener('click', function (e) {
+  for (const sm of $saveDraftButton) {
+    if (e.target === sm) {
+      e.preventDefault();
+      // eslint-disable-next-line no-undef
+      const newEntry = new Entry();
+      newEntry.title = date;
+      for (const item of $fiveThings) {
+        const txt = item.value;
+        newEntry.fiveThings.push(txt);
+        newEntry.formattedDate = formattedDate;
+      }
+      // eslint-disable-next-line no-undef
+      drafts.push(newEntry);
+      showPage($homeDiv, $gratefulDiv);
+      $5thingsForm.reset();
+      break;
+    }
+  }
+});
 
-// $nJContButton.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   showPage($NJDiv, $gratefulDiv);
-//   const newEntry = new Entry();
-//   newEntry.title = date;
-//   newEntry.formattedDate = formattedDate;
-//   for (const item of $fiveThings) {
-//     const txt = item.value;
-//     newEntry.fiveThings.push(txt);
-//   }
-//   currentObj = newEntry;
-// });
+$nJContButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  showPage($NJDiv, $gratefulDiv);
+  // eslint-disable-next-line no-undef
+  const newEntry = new Entry();
+  newEntry.title = date;
+  newEntry.formattedDate = formattedDate;
+  for (const item of $fiveThings) {
+    const txt = item.value;
+    newEntry.fiveThings.push(txt);
+  }
+  // eslint-disable-next-line no-undef
+  currentObj = newEntry;
+});
 
-// $headerLogo.addEventListener('click', function (e) {
-//   const newEntry = new Entry();
-//   newEntry.title = date;
-//   newEntry.formattedDate = formattedDate;
-//   for (const item of $fiveThings) {
-//     const txt = item.value;
-//     newEntry.fiveThings.push(txt);
-//   }
-//   drafts.push(newEntry);
-//   $5thingsForm.reset();
-//   removePageID();
-//   for (const page of $pgList) {
-//     showPage($homeDiv, page);
-//   }
-//   window.alert('Your journal entry was saved as a draft!');
-// });
+$headerLogo.addEventListener('click', function (e) {
+// eslint-disable-next-line no-undef
+  const newEntry = new Entry();
+  newEntry.title = date;
+  newEntry.formattedDate = formattedDate;
+  for (const item of $fiveThings) {
+    const txt = item.value;
+    newEntry.fiveThings.push(txt);
+  }
+  // eslint-disable-next-line no-undef
+  drafts.push(newEntry);
+  $5thingsForm.reset();
+  removePageID();
+  for (const page of $pgList) {
+    showPage($homeDiv, page);
+  }
+  window.alert('Your journal entry was saved as a draft!');
+});
 
-// $doneButton.addEventListener('click', function (e) {
-//   if (($NJTextCont.value.length) > 0) {
-//     e.preventDefault();
-//     currentObj.text = $NJTextCont.value;
-//     const instanceVar = currentObj;
-//     sendMoodReq(currentObj.text);
-//   } else {
-//     window.alert("It looks like you forgot to write something. Tell us what's on your mind! (or save it as a draft for later)");
-//   }
-// });
+$doneButton.addEventListener('click', function (e) {
+  if (($NJTextCont.value.length) > 0) {
+    e.preventDefault();
+    // eslint-disable-next-line no-undef
+    currentObj.text = $NJTextCont.value;
+    // eslint-disable-next-line no-undef
+    sendMoodReq(currentObj.text);
+  } else {
+    window.alert("It looks like you forgot to write something. Tell us what's on your mind! (or save it as a draft for later)");
+  }
+});
 
-// $homeButton.addEventListener('click', function (e) {
-//   showPage($homeDiv, $modalDiv);
-// });
+$homeButton.addEventListener('click', function (e) {
+  showPage($homeDiv, $modalDiv);
+});
